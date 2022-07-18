2a0585f078eca1011efa576c9b1c15787c149b5a
Jeremie Pilotte <Jeremie.p23@gmail.com>
2022-07-18 19:23:36 -0400

    doesnt worl

--- main.c
+++ main.c
@@ -23,6 +23,7 @@ void floor_print(void);
 
 void obstical_print(void);
 
+
 void map_init(void);
 
 void player_init(void);
@@ -36,10 +37,15 @@ void window(void)
     void	*mlx;
 	void	*mlx_win;
 	t_data	img;
+	int x;
+	int y;
+
+	x = 500;
+	y = 500;
 	
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "So_long my homie");
-	img.img = mlx_new_image(mlx, 1920, 1080);
+	mlx_win = mlx_new_window(mlx, x, y, "So_long my homie");
+	img.img = mlx_new_image(mlx, x, y);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 								&img.endian);
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
