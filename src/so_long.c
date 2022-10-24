2ecb0770fe20c48de80419a1b51b61485fd492c6
Jeremie Pilotte <Jeremie.p23@gmail.com>
2022-10-24 16:57:42 -0400

    .

--- src/so_long.c
+++ src/so_long.c
@@ -27,8 +27,8 @@ void get_map_size(t_data *data, char argv[1])
     i = 0;
     fd = open(argv, O_RDONLY);
     rows = get_next_line(fd);
-    data->height = 1;
-    data->width = 1;
+    data->height = 0;
+    data->width = -1;
     while (rows[i])
     {
         i++;
@@ -46,16 +46,15 @@ void get_map_size(t_data *data, char argv[1])
     close(fd);
 }
 
-void render_images(char argv[1])
-{
-    int fd;
-
-    fd = open(argv, O_RDONLY);
-    
-}
+//void render_images(char argv[1]);
 
 int main(int argc, char **argv)
 {
+    int x;
+    int y;
+
+    x = 0;
+    y = 0;
     t_data *data;
     data = get_data();
     if (parse_for_init(argc, argv[1]) == 1)
@@ -63,8 +62,17 @@ int main(int argc, char **argv)
         init_data(*argv, argc);
         get_map_size(data, argv[1]);
         data->win = mlx_new_window(data->mlx, data->width, data->height, "so_long");
-        render_images(argv[1]);
-        mlx_put_image_to_window(data->mlx, data->win, data->wall, 0, 0);
+        //render_images(argv[1]);
+        while (x != data->width)
+        {
+            mlx_put_image_to_window(data->mlx, data->win, data->wall, x, 0);
+            x += 32;
+        }
+        while (y != data->height)
+        {
+            mlx_put_image_to_window(data->mlx, data->win, data->wall, 0, y);
+            y += 32;
+        }
         mlx_put_image_to_window(data->mlx, data->win, data->floor, 32, 32);
         mlx_put_image_to_window(data->mlx, data->win, data->collectable, 32, 32);
         mlx_loop(data->mlx);
